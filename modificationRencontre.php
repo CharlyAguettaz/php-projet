38f2146af6ab15610dc03270ca5a69307aac96a4
CharlyAguettaz <55875854+CharlyAguettaz@users.noreply.github.com>
2021-01-02 18:35:26 +0100

    rencontre2

---
+++ modificationRencontre.php
@@ -0,0 +1,29 @@
+<?php
+    if (!empty($_POST['date']) && !empty($_POST['heure']) && !empty($_POST['adversaire']) && !empty($_POST['lieu'])) {
+        $id_rencontre = htmlentities($_POST['Id_rencontre']);
+        $date = htmlentities($_POST['date']);
+        $heure = htmlentities($_POST['heure']); 
+        $adversaire = htmlentities($_POST['adversaire']);
+        $lieu = htmlentities($_POST['lieu']);
+        $points_equipe = htmlentities($_POST['points_equipe']);
+        $points_adversaire = htmlentities($_POST['points_adversaire']);
+        
+        $db = 'football';
+        $login = 'root';
+        $mdp = '';
+        try {
+            $linkpdo = new PDO("mysql:host=localhost;dname=$db",$login,$mdp);
+        }
+        catch (Exeption $e) {
+             die('Error :' . $e->getMessage());
+        }
+
+        $req = $linkpdo->prepare("UPDATE football.rencontre SET Date_rencontre = :date, Heure_rencontre = :heure, Nom_adversaire = :adversaire, Lieu_de_rencontre = :lieu, Points_equipe = :points_equipe, Points_adversaire = :Points_adversaire WHERE Id_rencontre = :Id_rencontre");
+        $req->execute(array('Id_rencontre' => $id_rencontre, 'date' => $date, 'heure' => $heure, 'adversaire' => $adversaire,  'lieu' => $lieu, 'points_equipe' => $points_equipe, 'points_adversaire' => $points_adversaire));
+        if ($req != FALSE) {
+            print("Modification effectué avec succés");
+        } else {
+            print("Erreur execute");
+        }
+    }
+?>
--- modifierRencontre.php
+++ modifierRencontre.php
@@ -9,43 +9,25 @@
         catch (Exeption $e) {
              die('Error :' . $e->getMessage());
         }
-        $req = $linkpdo->prepare("SELECT * FROM football.players WHERE numLicence LIKE ?");
+        $req = $linkpdo->prepare("SELECT * FROM football.rencontre WHERE Id_rencontre LIKE ?");
         $req->execute(array(htmlentities($_POST['id'])));
         $res=$req->fetch();
-    }
-    if (!empty($_POST['date']) && !empty($_POST['heure']) && !empty($_POST['adversaire']) && !empty($_POST['lieu'])) {
-        $date = htmlentities($_POST['date']);
-        $heure = htmlentities($_POST['heure']); 
-        $adversaire = htmlentities($_POST['adversaire']);
-        $lieu = htmlentities($_POST['lieu']);
-        $points_equipe = htmlentities($_POST['points_equipe']);
-        $points_adversaire = htmlentities($_POST['points_adversaire']);
-        
-
-        $req2 = $linkpdo->prepare("UPDATE football.rencontre SET nom = :nom, prenom = :prenom, photo = :photo, dateDeNaissance = :dateDeNaissance, taille = :taille, poids = :poids, postePrefere = :postePrefere WHERE numLicence = :numLicence");
-        $req2->execute(array('nom' => $nom, 'prenom' => $prenom, 'photo' => $photo, 'dateDeNaissance' => $dateDeNaissance,  'taille' => $taille, 'poids' => $poids, 'postePrefere' => $postePrefere, 'statut' => $statut, 'numLicence' => $numLicence));
-        $res=$req2->fetch();
-        if ($req2 != FALSE) {
-            print("Modification effectué avec succés");
-        } else {
-            print("Erreur execute");
-        }
     }
 ?>
 
 <html>
     <body>  
         <p>
-            <a href="http://localhost/php-projet/ajoutJoueur.php">Ajouter un joueur</a> /
+            <a href="http://localhost/php-projet/rencontre.php">Rencontre</a> /
             <a href="http://localhost/php-projet/recherche.php">Rechercher un joueur</a>
         </p>            
         <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
-            Date : <input type="date" name="date"><br />
-            Heure : <input type="text" name="heure"><br />
-            Adversaire : <input type="text" name="adversaire" ><br />
-            Lieu : <input type="text" name="lieu"><br />
-            But Pour : <input type="number" name="points_equipe" ><br />
-            But Contre : <input type="number" name="points_adversaire" ><br />
+            Date : <input type="date" value="<?php echo $res['Date_rencontre'] ?>" name="date"><br />
+            Heure : <input type="text" value="<?php echo $res['Heure_rencontre'] ?>" name="heure"><br />
+            Adversaire : <input type="text" value="<?php echo $res['Nom_adversaire'] ?>" name="adversaire" ><br />
+            Lieu : <input type="text" value="<?php echo $res['lieu_de_rencontre'] ?>" name="lieu"><br />
+            But Pour : <input type="number" value="<?php echo $res['Points_equipe'] ?>" name="points_equipe" ><br />
+            But Contre : <input type="number" value="<?php echo $res['Points_adversaire'] ?>" name="points_adversaire" ><br />
 
             <input type='Submit' value='Valider'>
         </form>

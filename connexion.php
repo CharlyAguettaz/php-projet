29fdf9f2d351cdaf5cdc6a0d7ef553e043ee7580
Anthony <anthony.aymeric13@gmail.com>
2021-01-02 16:40:11 +0100

    Update connexion.php

---
+++ connexion.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="<?php $_SERVER['PHP_SELF']?>" method='post'>
+            Identifiant : <input type='email' required name='username'>
+            Mot de passe : <input type='password' required name='password'>
+            <input type='submit' value='Valider'>
+        </form>
+    </body>
+</html>
